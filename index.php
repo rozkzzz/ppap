16956ac678d564c151390818ff1b9f040e10f045
unknown <tharit426@gmail.com>
2025-10-31 01:18:12 +0700

    website update

--- index.php
+++ index.php
@@ -6,17 +6,23 @@
 
 <body style="background-color:#27F5DA">
        
-    <div><h1>ยืมคืนอุปกรณ์การแพทย์และอุปกรณ์เครื่องช่วยความพิการ</h1></div>
+    
     
     <center><img src="logo_pao.png" width="450"></center>
     <form>
+    
     <div class="login_ip">
-        <input type="text" placeholder="USERNAME" name="username">
+    <center>
+    <div class="login_title"><h1>ยืมคืนอุปกรณ์การแพทย์และอุปกรณ์เครื่องช่วยความพิการ</h1></div><br>
+    </center>
+    <input type="text" placeholder="USERNAME" name="username">
         <input type="password" placeholder="PASSWORD" name="password"> 
     </div>
     
     <div class="login_btn">
-        <button type="submit" name="login">LOG IN</button>
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        LOG IN
+        </button>
     </div>
     </form>
 
